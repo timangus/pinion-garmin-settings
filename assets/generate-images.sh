47b25a2deb24b451f90d5fa29764b32b99987822
Tim Angus <tim@ngus.net>
2025-07-01 20:32:37 +0100

    Add resources-edge1040

--- assets/generate-images.sh
+++ assets/generate-images.sh
@@ -41,5 +41,5 @@ generate_images()
 }
 
 generate_images connecting_image.png true 250x250 200x200 150x150
-generate_images connecting_image.png false 24x24 35x35 41x58
+generate_images connecting_image.png false 24x24 35x35 40x40 41x58
 generate_images trigger_image.png true 250x250 200x200 150x150
