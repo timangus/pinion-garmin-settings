82305234df5de7dedc9bf34265b5bb2cee6be2aa
Tim Angus <tim@ngus.net>
2025-07-03 11:41:39 +0100

    Combine shell scripts

--- generate-device-resources.sh
+++ generate-device-resources.sh
@@ -16,6 +16,13 @@ declare -A LINK_TO_ASSET_BASE=(
   ["scan_icon.png"]="connecting_image"
 )
 
+BLACK_BG_SIZES=(
+  "400x400"
+  "250x250"
+  "200x200"
+  "150x150"
+)
+
 read -r -d '' INPUT_DATA << EOM
 edge530 200x200 35x35 24x24
 edge540 150x150 35x35 41x58
@@ -28,6 +35,43 @@ edge1050 400x400 68x68 80x118
 edgeexplore2 200x200 36x36 24x24
 edgemtb 150x150 36x36 40x66
 EOM
+
+generate_image()
+{
+  local input="$1"
+  local size="$2"
+  local basename="${input%.*}"
+  local output="${ASSETS_DIR}/${basename}-${size}.png"
+
+  local apply_bg="false"
+  for bg_size in "${BLACK_BG_SIZES[@]}"; do
+    if [[ "$size" == "$bg_size" ]]; then
+      apply_bg="true"
+      break
+    fi
+  done
+
+  if [[ -f "$output" ]]; then
+    return
+  fi
+
+  if [[ ! -f "$ASSETS_DIR/$input" ]]; then
+    echo "  Error: Source file '$ASSETS_DIR/$input' not found" >&2
+    return 1
+  fi
+
+  echo "  Generating $output"
+
+  if [[ "$apply_bg" == "true" ]]; then
+    convert "$ASSETS_DIR/$input" -background black -flatten \
+      -resize "$size" -gravity center -extent "$size" \
+      -strip "$output"
+  else
+    convert "$ASSETS_DIR/$input" -background none \
+      -resize "$size" -gravity center -extent "$size" \
+      -strip "$output"
+  fi
+}
 
 while read -r line; do
   [[ "$line" =~ ^#.*$ || -z "$line" ]] && continue
@@ -49,8 +93,11 @@ while read -r line; do
     if [[ -n "${LINK_NAMES[$i]}" ]]; then
       for link_name in ${LINK_NAMES[$i]}; do
         asset_base="${LINK_TO_ASSET_BASE[$link_name]}"
+        asset_input="${asset_base}.png"
         asset_file="$ASSETS_DIR/${asset_base}-${size}.png"
         target_link="$device_dir/$link_name"
+
+        generate_image "$asset_input" "$size"
 
         if [[ -f "$asset_file" ]]; then
           ln -sf "../../$asset_file" "$target_link"
